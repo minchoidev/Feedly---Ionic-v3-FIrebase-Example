7e4284c28fede02dd5c878cfb00c6f269fb2e8b7
Minseok Choi <choim@interdev.ca>
2020-03-09 17:56:18 -0400

    Add Signup Function with Firebase

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-signup',
@@ -7,11 +8,35 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
+  name: string = "";
+  email: string = "";
+  password: string = "";
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupPage');
+  }
+
+  signup() {
+    console.log(this.name, this.email, this.password);
+    firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then((data)=>{
+      
+      let newUser: firebase.User = data.user;
+      newUser.updateProfile({
+        displayName: this.name,
+        photoURL: ""
+      }).then(() => {
+        console.log("Profile Updated")  // here doesn't reutnr any value, so just put a string value as log
+      }).catch((err) => {
+        console.log(err)
+      })
+
+      console.log(data);
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
   goBack() {
