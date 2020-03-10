1a0981b1dc9db61f92e6c81ace3906956cfa3498
Minseok Choi <choim@interdev.ca>
2020-03-10 13:46:39 -0400

    Add login Firebase Auth

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SignupPage } from '../signup/signup';
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-login',
@@ -8,7 +9,25 @@ import { SignupPage } from '../signup/signup';
 })
 export class LoginPage {
 
+  email: string = "";
+  password: string = "";
+
   constructor(public navCtrl: NavController) {
+
+  }
+  
+  login() {
+    console.log("login trial");
+    
+    firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+    .then((user) => {
+      debugger;
+      console.log("user: ")
+      console.log(user)
+    }).catch((err) => {
+      debugger;
+      console.log(err)
+    })
 
   }
 
